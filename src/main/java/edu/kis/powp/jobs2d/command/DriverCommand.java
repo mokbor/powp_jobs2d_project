3418ed47ec7eedecb5a5721f1255ef4e60f3f148
mokbor <43931480+mokbor@users.noreply.github.com>
2019-10-27 17:21:42 +0100

    Command 3.1.1

---
+++ src/main/java/edu/kis/powp/jobs2d/command/DriverCommand.java
@@ -0,0 +1,9 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public interface DriverCommand {
+	
+	public void execute();
+
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/command/OperatoToCommand.java
@@ -0,0 +1,13 @@
+package edu.kis.powp.jobs2d.command;
+
+public class OperatoToCommand implements DriverCommand {
+	
+	private int x,y;
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
